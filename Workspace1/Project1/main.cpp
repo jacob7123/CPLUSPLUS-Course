f8e159e0157c7f06596fdface586b35ea4ec0ca3
Jacob Huang <h39713971@gmail.com>
2024-01-02 23:30:14 -0800

    Environment Setup

---
+++ Workspace1/Project1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "I am Jacob" << endl;
+    return 0;
+}
---
+++ Workspace1/Project2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "I am Jacob from Project2" << endl;
+    return 0;
+}
