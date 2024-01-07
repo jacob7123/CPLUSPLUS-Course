7e9941863e26d8dcf7d7013106cbbfcdc2219000
Jacob Huang <h39713971@gmail.com>
2024-01-06 23:18:14 -0800

    First Program

---
+++ Section4/FirstProgram/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+//using namespace std;
+
+int main(){
+    int love_number;
+    std::cout << "Enter you favorite number between 1 to 100." << std::endl;
+    
+    std::cin >> love_number;
+    
+    std::cout << "Your love number is: " << love_number << std::endl;
+    return 0;
+}
